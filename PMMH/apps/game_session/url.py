42ae343032b0b8234de7405552e5e62337560e76
Özay <ozay_ezerceli804@hotmail.com>
2019-11-20 23:28:48 +0300

    city-view, character-view, religion pages created (#3)
    those pages created with html and css

--- PMMH/apps/game_session/url.py
+++ PMMH/apps/game_session/url.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.city_view, name='city_view')
 ]
--- PMMH/apps/game_session/views.py
+++ PMMH/apps/game_session/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from .models import GameSession
 
 # Create your views here.
-def index(requst):
+def city_view(request):
+    return render(request, 'overworld/city_view.html', {})
+    
+def index(request):
     game_sessions = GameSession.objects.order_by('users')
     context = {
         'game_sessions': game_sessions,
     }
-    return render(requst, 'game_session/index.html', context)
+    return render(request, 'overworld/city_view.html', context)
+
+
